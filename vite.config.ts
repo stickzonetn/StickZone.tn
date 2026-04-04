259fbc98e825b305795c2d5caed6ecf7fa87ab3b
stickzonetn <stickzone.tn@gmail.com>
2026-04-04 20:37:18 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/StickZone.tn/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
